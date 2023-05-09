0db2262459fc10c7c662c326e94198c33cb9b054
Yasser Greyeb <Yasser.Greyeb@gmail.com>
2023-05-09 10:03:29 -0500

    Add daily exercise

--- leetcode-solutions/src/main/java/io/nuevedejun/leetcode/spiralorder/Solution.java
+++ leetcode-solutions/src/main/java/io/nuevedejun/leetcode/spiralorder/Solution.java
@@ -1,9 +1,76 @@
 package io.nuevedejun.leetcode.spiralorder;
 
+import java.util.AbstractList;
 import java.util.List;
 
 public class Solution {
   public List<Integer> spiralOrder(int[][] matrix) {
-    return List.of();
+    return new SpiralList(matrix);
+  }
+
+  @SuppressWarnings("java:S2160")
+  private static final class SpiralList extends AbstractList<Integer> implements List<Integer> {
+    private final int[][] matrix;
+    private final int m;
+    private final int n;
+    private final int size;
+    private final int[] back;
+    private boolean init = false;
+
+    public SpiralList(int[][] matrix) {
+      this.matrix = matrix;
+      this.m = matrix.length;
+      this.n = matrix[0].length;
+      this.size = m * n;
+      this.back = new int[size];
+    }
+
+    @Override
+    public Integer get(int index) {
+      if (index < 0 || index >= size) throw new IndexOutOfBoundsException(index);
+      if (!init) initialize();
+      return back[index];
+    }
+
+    @SuppressWarnings("java:S3776")
+    private void initialize() {
+      int idx = 0;
+      int top = 0;
+      int bottom = m - 1;
+      int left = 0;
+      int right = n - 1;
+      while (top <= bottom && left <= right) {
+        //  right
+        for (int i = left; i <= right; i++) {
+          back[idx++] = matrix[top][i];
+        }
+        top++;
+        //  down
+        for (int i = top; i <= bottom; i++) {
+          back[idx++] = matrix[i][right];
+        }
+        right--;
+        //  left
+        if (top <= bottom) {
+          for (int i = right; i >= left; i--) {
+            back[idx++] = matrix[bottom][i];
+          }
+          bottom--;
+        }
+        //  up
+        if (left <= right) {
+          for (int i = bottom; i >= top; i--) {
+            back[idx++] = matrix[i][left];
+          }
+          left++;
+        }
+      }
+      init = true;
+    }
+
+    @Override
+    public int size() {
+      return size;
+    }
   }
 }
--- leetcode-solutions/src/test/java/io/nuevedejun/leetcode/spiralorder/SolutionTest.java
+++ leetcode-solutions/src/test/java/io/nuevedejun/leetcode/spiralorder/SolutionTest.java
@@ -11,19 +11,19 @@ class SolutionTest {
 
   @Test
   void spiralOrder0() {
-    assertEquals(List.of(1, 2, 3, 6, 9, 8, 7, 4, 5), new int[][]{
+    assertEquals(List.of(1, 2, 3, 6, 9, 8, 7, 4, 5), solution.spiralOrder(new int[][]{
         new int[]{1, 2, 3},
         new int[]{4, 5, 6},
         new int[]{7, 8, 9}
-    });
+    }));
   }
 
   @Test
   void spiralOrder1() {
-    assertEquals(List.of(1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7), new int[][]{
+    assertEquals(List.of(1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7), solution.spiralOrder(new int[][]{
         new int[]{1, 2, 3, 4},
         new int[]{5, 6, 7, 8},
         new int[]{9, 10, 11, 12}
-    });
+    }));
   }
 }
